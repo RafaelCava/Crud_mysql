f71e548f7daa3b6dd372945e89bb770eb58c1701
Rafael <ra.facavalcante@hotmail.com>
2021-08-18 17:21:39 -0300

    s: mudando estrutura

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
-const { Post } = require('./middlewares/middlewares');
-const routes = require('./routes/routes');
+const { Post } = require('./src/middlewares/middlewares');
+const routes = require('./src/routes/routes');
 const app = express()
 const port = 8000
 
