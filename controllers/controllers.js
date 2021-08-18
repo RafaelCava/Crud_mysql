7bb5d03f7122f5e31f6af562908e4b3ecbb4e6ca
Rafael <ra.facavalcante@hotmail.com>
2021-08-18 11:01:51 -0300

    criando controllers

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,30 +1,45 @@
 const db = require('../data/db');
-(async () => {
+
+
+/* (async () => {
   console.log("passou por aqui")
   // await db.insertCustomers({ nome: "zé", idade: 18, uf: "SC" })
   // await db.updateCustomer(3, { nome: "zézim", idade: 18, uf: "SCc" })
   await db.deleteCustomer(12)
   const clientes = await db.selectCustomers();
   console.log(clientes);
-})()
+})() */
 
 
-
-
-/*
-const getClientes = async () => {
-  // const db = require('./db');
-  console.log("passou por aqui")
-  const clientes = await db.selectCustomers();
-  clientes.forEach(val => console.log(val))
+const getClientes = async (req, res) => {
+  try {
+    const clientes = await db.selectCustomers();
+    res.status(200).json(clientes)
+  } catch (err) {
+    console.error(err);
+    res.status(404).json({ message: err })
+  }
 }
 
-getClientes() */
-/*
-const postCliente = async (cliente) => {
-  const db = require('./db');
-  console.log("passou por aqui")
-  await db.insertCustomers(cliente)
+const postCliente = async (req, res) => {
+  const { nome, idade, uf } = req.body;
+  try {
+    await db.insertCustomers({ nome, idade, uf })
+    res.status(201).json({ message: 'Cliente criado com sucesso' })
+  } catch (err) {
+    console.error(err);
+    res.status(404).json({ message: err })
+  }
 }
 
-postCliente({ nome: "zé", idade: 283, uf: "SC" }) */
+const deleteCliente = async (req, res) => {
+  const { id } = req.params;
+  try {
+    await db.deleteCustomer(id)
+    res.status(200).json({ message: 'Cliente deletado com sucesso' })
+  } catch (err) {
+    console.error(err);
+    res.status(404).json({ message: err })
+  }
+}
+
