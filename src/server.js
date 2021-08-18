879e6509345d9296029705cbfbb9d18478a240b4
Rafael <ra.facavalcante@hotmail.com>
2021-08-18 17:24:20 -0300

    s: estrutura de dados

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
-const { Post } = require('./src/middlewares/middlewares');
-const routes = require('./src/routes/routes');
+const { Post } = require('./middlewares/middlewares');
+const routes = require('./routes/routes');
 const app = express()
 const port = 8000
 
 app.use(express.json())
 app.use(Post)
 app.use('/api', routes)
-app.use('/explorer', swaggerUi.serve, swaggerUi.setup(require('./swagger/swagger.json')))
+app.use('/explorer', swaggerUi.serve, swaggerUi.setup(require('../swagger/swagger.json')))
 app.listen(port)
